7339b3dee60600ee5edebac73fa71399ba7c6c5e
tysunday <120739860+tysunday@users.noreply.github.com>
2023-01-22 21:50:52 +0500

    Add files via upload

---
+++ homework_21.01.2023_3.cpp
@@ -0,0 +1,19 @@
+﻿
+// Напишите программу, которая запрашивает два целых числа
+// x и y, после чего вычисляет и выводит значение x в степени y.
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x = 0, y = 0, w = 0;
+	cout << "Введите число: "; cin >> x; cout << endl;
+	cout << "Введите в какую степень хотите его возвести: "; cin >> y; cout << endl;
+
+	w = pow(x, y);
+
+		cout << w;
+}
+
