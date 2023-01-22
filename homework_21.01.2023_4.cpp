0e3721c16c2371af85c09a120758888e8e88a244
tysunday <120739860+tysunday@users.noreply.github.com>
2023-01-22 22:02:17 +0500

    Add files via upload

---
+++ homework_21.01.2023_4.cpp
@@ -0,0 +1,22 @@
+﻿// Найти среднее арифметическое всех целых чисел от 1 до 1000.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    
+    int number = 1, sumNumber = 0;
+    
+    for (number; number <= 1000; number++)
+    {
+        sumNumber = sumNumber + number;
+        cout << sumNumber << "\n";
+        // строчкой выше cout выводит на экране весь процесс суммирования чисел от 1 до 1000.
+    }
+
+    cout << sumNumber / 1000; 
+    // этот cout выводит необходимый нам результат т.е. среднее арифметическое суммы от 1 до 1000.
+}
+
