fa7915fc6652ef6dbfee3031bca15708947b91ac
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-01 21:36:00 +0500

    Add files via upload

---
+++ massive.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+const int N = 10;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int averageTemp = 0, countDays = 0, containerSum = 0, downAverageTemp = 0, b = 0, x = 0;
+    int days[N] = { -17, -23, -13, -15,
+        2, -11, -15, -25, -27, -20 };
+
+    for (int i = 0; i < N; i++)
+    {
+        containerSum += days[i];
+        averageTemp = containerSum / N;
+
+        if (i >= 9) {
+            while (b <= N)
+            {
+                if (averageTemp < days[x])
+                {
+                    downAverageTemp++;
+                }
+                x++;
+                b++;
+            }
+        }
+    }
+    cout << averageTemp << endl;
+    cout << downAverageTemp;
+}
+
