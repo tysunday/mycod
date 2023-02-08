e8e03eeaa27ce476cff787e9b1490f4e27b09d59
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-08 19:18:34 +0500

    Add files via upload

---
+++ classwork_06.02.2023_massiveDvumernie.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	const int column = 2; // переменные отвечающие за колонки и строки в двумерном массиве.
+	const int row = 2; /*и строки в двумерном массиве.*/
+
+	int arr[column][row] = { { 2, 3 }, {4, 7} };
+
+	for (int i = 0; i < column; i++)
+	{
+		for (int j = 0; j < row; j++)
+		{
+			cout << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
+
+
