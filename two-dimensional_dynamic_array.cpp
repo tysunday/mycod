0dd426474ceb2c808cab8672b9af5c7c8635d71a
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-12 16:54:08 +0500

    Add files via upload

---
+++ two-dimensional_dynamic_array.cpp
@@ -0,0 +1,29 @@
+﻿// two-dimensional_dynamic_array.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int size = 0;
+	cout << "enter array size" << endl;
+	cin >> size;
+
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		arr[i] = rand() % 10;
+	}
+
+	for (int i = 0; i <=  size; i++)
+	{
+		cout << arr[i] << "\t";
+
+		cout << arr + i << endl; // здесь будут выводиться адреса оперативной памяти
+	}
+	delete [] arr;
+}
+
---
+++ two-dimensional_dynamic_array_2.cpp
@@ -0,0 +1,56 @@
+﻿// two-dimensional_dynamic_array_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int rows = 0;
+	int cols = 0;
+	cout << "enter rows counst" << endl;
+
+	cout << "enter colums count" << endl;
+
+	cin >> rows;
+	cin >> cols;
+
+	int** arr = new int* [rows]; // здесь массив указателей на массивы.
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int [cols];
+
+	}
+
+	for (int i = 0; i < rows; i++)
+	{
+		for (int j = 0; j < cols; j++)
+		{
+			arr[i][j] = rand() % 20;
+		}
+	}
+	//////////////////////////////////////////////////
+
+	 for (int i = 0; i < rows; i++)
+	{
+		 for (int j = 0; j < cols; j++)
+		 {
+			 cout << arr[i][j] << "\t";
+		 }
+		 cout << endl;
+	}
+
+	//////////////////////////////////////////////////
+
+	for (int i = 0; i < rows; i++)
+	{
+		delete [] arr[i];
+	} // чистим память сначала в подмассивах
+
+		delete[] arr; // потом чистим память массива основного (с указателями на подмассивы)
+
+
+
+}
+
