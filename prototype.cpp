7f8c10bf4ce2b6f35807d50c4dbf27c23e52e07b
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-15 20:48:26 +0500

    Add files via upload

---
+++ prototype.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+void Hello();
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int sizeA = 5;
+	int arraryTest[sizeA];
+
+	Hello();
+}
+
+void Hello()
+{
+	cout << "Hello";
+}
