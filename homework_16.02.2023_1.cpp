8d4364b6c4cb6b1719df2f5f48409e344e49dce7
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-16 21:38:57 +0500

    Add files via upload

---
+++ homework_16.02.2023_1.cpp
@@ -0,0 +1,29 @@
+﻿// Даны  два действительных числа х и у. Проверьте принадлежит ли точка с координатами (х, у) заштрихованному квадрату 
+// (включая его границу). Если точка принадлежит квадрату, введите слово YES, иначе  выведите слово NO.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float Belong(double x, double y)
+{
+	float result = (abs(x + y));
+	return result;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	double x = 0, y = 0;
+	cout << "Введите значения: " << endl;
+	cin >> x >> y;
+
+	float sum = Belong(x, y);
+	cout << sum << endl;
+
+	if (sum <= 1)
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+
+}
