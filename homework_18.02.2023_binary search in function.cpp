42454861b6b2c4b09d1f33711f1c352172998a1f
tysunday <120739860+tysunday@users.noreply.github.com>
2023-02-19 00:33:48 +0500

    Add files via upload

---
+++ homework_18.02.2023_binary search in function.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <time.h>
+#include <algorithm>
+using namespace std;
+
+inline int BinarySearch(int left, int right, int mid, int array[], int number, bool have, int size)
+{
+
+
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rand() % 10;
+		cout << array[i] << " ";
+		if ((i + 1) % (size / 2) == 0)
+		{
+			cout << endl;
+		}
+	} 
+	cout << endl << endl;
+
+	sort(array, array + size);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << " ";
+		if ((i + 1) % (size / 2) == 0)
+		{
+			cout << endl;
+		}
+	}
+
+	cout << endl;
+	cout << endl;
+
+
+	while ((left <= right) && (have != 1))
+	{
+		mid = (left + right) / 2;
+		if (array[mid] == number) have = 1;
+		if (array[mid] > number) right = mid - 1;
+		else left = mid + 1;
+		cout << mid << endl;
+	}
+
+		if (have == 1) cout << "Индекс элемента " << number << " равен " << mid + 1;
+		else cout << "такого элемента нет" << endl;
+
+	return 0;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+
+
+	bool have = 0;
+	int size = 0, key = 0, number = 0;
+
+	cout << "Введите размер массива " << "\n";
+	cin >> size;
+	cout << "Ключ какого числа в массиве вы хотите найти? " << "\n";
+	cin >> number;
+
+	int left = 0, right = size, mid = 0;
+
+	int* array = new int[size];
+
+int res = BinarySearch(left, right, mid, array, number, false, size);
+}
---
+++ homework_18.02.2023_lineSearch.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	int size = 0, key = 0, number = 0;
+	bool have = 0;
+	cout << "Введите размер массива " << "\n";
+	cin >> size;
+
+	int* array = new int[size];
+
+	for (int i = 0; i < size; i++)
+	{
+		array[i] = rand() % 10;
+		cout << array[i] << " ";
+		if (i == 9)
+		{
+			cout << endl;
+		}
+	}
+	cout << endl;
+	cout << "Ключ какого числа в массиве вы хотите найти? " << "\n";
+	cin >> number;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] == number)
+		{
+			cout << "Ключ находится в ячейке: " << i + 1 << "\n";
+			have = 1;
+		}
+		else if (have == 0 && i + 1 == size)
+		{
+			cout << "ключа нету";
+		}
+	}
+}
