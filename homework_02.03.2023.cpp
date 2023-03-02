14c6a9062292d9e8904528e658fd9582408d50cf
tysunday <120739860+tysunday@users.noreply.github.com>
2023-03-02 22:06:02 +0500

    Add files via upload

---
+++ homework_02.03.2023.cpp
@@ -0,0 +1,88 @@
+﻿//Задание 2. Написать функцию, которая удаляет из строки
+//все вхождения в нее заданного символа.
+//Задание 3. Написать функцию, которая вставляет в строку
+//в указанную позицию заданный символ.
+
+#include <iostream>
+#include <string>
+#include <windows.h> // для вывода ввода в консоль русских символов
+
+using namespace std;
+
+int replaceAllSymbol(string sentence);
+int inputSomeSymbol(string sentence);
+int ReplacePointerToExclamation(string sentence);
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string sentence = "";
+	
+
+	replaceAllSymbol(sentence);
+	inputSomeSymbol(sentence);
+	ReplacePointerToExclamation(sentence);
+}
+
+int replaceAllSymbol(string sentence)
+{
+	char symbol = '0';
+	cout << "input sentence: \n";
+	getline(cin, sentence);
+	cout << sentence << " \n";
+
+	cout << "what is symbol you wanna replace? \n";
+	cin >> symbol;
+
+	cout << endl;
+	for (int i = 0; i < sentence.length(); i++)
+	{
+		if (sentence[i] == symbol)
+		{
+			sentence[i] = 0;
+		}
+		cout << sentence[i];
+	}
+	cout << endl;
+	return 0;
+}
+
+int inputSomeSymbol(string sentence)
+{
+	char symbol = '0';
+	int position = 0;
+
+	cout << "input sentence: \n";
+	getline(cin, sentence);
+	cout << sentence << " \n";
+
+	cout << "enter at which position you want to paste: \n";
+	cin >> position;
+	cout << "enter the symbol you want to insert: \n";
+	cin >> symbol;
+
+	sentence[position] = symbol;
+
+	cout << sentence << "\n";
+	return 0;
+}
+
+int ReplacePointerToExclamation(string sentence)
+{
+	cout << "input sentence: \n";
+	getline(cin, sentence);
+	cout << sentence << " \n";
+
+	for (int i = 0; i < sentence.length(); i++)
+	{
+		if (sentence[i] == 46)
+		{
+			sentence[i] = 33;
+		}
+		cout << sentence[i];
+	}
+	return 0;
+}
---
+++ homework_02.03.2023_string.cpp
@@ -0,0 +1,65 @@
+﻿//Задание 1. Написать функцию, которая удаляет из строки
+//символ с заданным номером.
+
+#include <iostream>
+#include <string>
+#include <windows.h> // для вывода ввода в консоль русских символов
+#include <time.h>
+#include <iomanip> // для функции setw которая ограничивает кол-во введёных символов и не даёт выйти за пределы массива.
+
+void CinClear(); // функция очищения буфера Cin
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int numberLetter = 0, size = 50;
+	string ch = "";
+	string sentence = "";
+	string Choose = "Да";
+
+	cout << "Введите предложение: \n";
+	getline(cin, sentence);
+
+	while (Choose != "Нет" && Choose == "Да") {
+		
+		cout << sentence << endl;
+
+		cout << " Какую по счёту букву вы хотите убрать? \n";
+		cin >> numberLetter;
+
+		while (numberLetter > sentence.length() || numberLetter <= 0)
+		{
+			cout << "Попробуйте ещё раз ваше число не подходит в диапозон количества букв в предложении \n";
+			cin >> numberLetter;
+		}
+
+		CinClear();
+		cout << " на что хотите заменить? \n";
+		getline(cin, ch);
+
+		string result = "";
+		result = sentence.replace(sentence.find(sentence[numberLetter - 1]), 1, ch);
+		cout << result << " \n";
+
+		cout << "Хотите поменять ещё какие-то буквы ? \n Да? Нет? \n";
+		cin >> Choose;
+	}
+
+
+}
+
+void CinClear()
+{
+	if (char(cin.peek()) == '\n')
+		cin.ignore();
+
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(32767, '\n');
+	}
+}
