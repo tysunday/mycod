f51b3e7818a3e7f579ed17f7946ec617238cebd1
tysunday <120739860+tysunday@users.noreply.github.com>
2023-03-17 21:34:32 +0500

    Add files via upload

---
+++ cw_17.03.2023.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <time.h>
+#include <windows.h>
+#include <cstdio>
+
+using namespace std;
+
+
+struct Test {
+	unsigned int Python: 15;
+	unsigned int JS: 2;
+	unsigned int PHP: 2;
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	FILE* myFile;
+	myFile = fopen("Текст.txt", "a");
+	if (myFile == NULL)
+		cout << "Ошибка при открытии файла";
+	else
+		cout << "Файл открыт";
+	
+	char text[40];
+	cin.get(text, 50);
+
+	fputs(text, myFile);
+	fclose(myFile);
+	
+	cout << myFile;
+
+}
