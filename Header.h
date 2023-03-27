87451a3bc1db03114f5c144cb5ed6e44f8a7bf85
tysunday <120739860+tysunday@users.noreply.github.com>
2023-03-27 17:42:00 +0500

    Add files via upload

--- Header.h
+++ Header.h
@@ -1,15 +1,22 @@
 #pragma once
+
 #include <iostream>
+#include <string>
 #include <windows.h>
-#include <time.h>
-#include <cmath>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
-void writeArray(int arr[], int size)
-{
-	srand(time(NULL));
-	for (int i = 0; i < size; i++)
-	{
-		arr[i] = 1 + rand() % 8;
-		cout << arr[i] << endl << endl;
-	}
-}
+//prototypes
+/*void CheckOpenFile(ofstream file);*/ // проверка открыт ли файл
+string CreatedFile(string path, ofstream fout); // для создания/открытия файла (с затиранием данных)
+void OpenFile(string path, ofstream &fout);  // если хотим открыть уже существующий файл и изменить информацию без затирания данных.
+string WriteInFile(ofstream &fout); // функция для записи информации в файл
+void ReadingFile(ifstream &fin); // функция для чтения данных в файлах
+void DeleteSomeString(ifstream &fin, string path, ofstream &fout); // функция для удаления последней строки (пока что последней, далее введу удаление любой указанной строки)
+void CopyFile(ifstream &fin, string path, ofstream &fout); // функция полностью копирует один файл в указанный пользователем файл
+void SearchMaxString(ifstream fin); // ищет самую длинную строку, выводит её на консоль, выводит количество символов и номер этой строки
+void SearchWord(); // ищет сколько раз встречается заданное пользователем слово
+void ChangeWord(); // заменяет все вхождения заданного пользователем слова на новое слово.
+void AddSomeText(); // добавить конкретный текст в конкретную строку
+
